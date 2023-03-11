9ed633412a64789277dfc1d9e93f6092f1f933c3
Amal Hassan <amalmhass@gmail.com>
2023-03-11 12:55:48 -0500

    bg color and object

--- script.js
+++ script.js
@@ -16,7 +16,12 @@ const poems = [
   },
 
   // YOUR CODE HERE: Add an object containing two key value pairs: author and poem. Set the author property as equal to a string with your first name. Set the poem property as equal to a short "roses are red" poem. Use the new line character as in the example when you would like a line break.
-];
+  {
+    author: "Amal",
+    poem: "Roses are red \n Violets are blue \n sheep go baah \n and cows go moo",
+    image: "/assets/sheep-and-cow.jpeg",
+  },
+  ];
 
 let displayPoems = (array) => {
   let html = "";
